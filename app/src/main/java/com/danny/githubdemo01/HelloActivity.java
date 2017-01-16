146cf7ec0793e74d16046f95600ebac747e4fa54
Danny <974156919@qq.com>
2017-01-16 15:54:46 +0800

    第一次修改

--- app/src/main/java/com/danny/githubdemo01/HelloActivity.java
+++ app/src/main/java/com/danny/githubdemo01/HelloActivity.java
@@ -9,5 +9,8 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        /**
+         * 此处为第一次修改
+         */
     }
 }
